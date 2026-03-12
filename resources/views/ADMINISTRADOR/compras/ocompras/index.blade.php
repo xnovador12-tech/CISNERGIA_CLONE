cbe2949e8c19fdb7226172fc8703a6225c2a808c
Josemaria <josemaria@cuanticagroup.com>
2026-03-11 22:56:10 -0500

    Revert "se eliminaron"
    This reverts commit 9c96a12ef13f9a9c03261a5701d8c53fc000b71e.

---
+++ resources/views/ADMINISTRADOR/compras/ocompras/index.blade.php
@@ -0,0 +1,260 @@
+@extends('TEMPLATES.administrador')
+
+@section('title', 'ORDEN DE COMPRAS')
+
+@section('css')
+<style>
+    /* Asegurar que Select2 dropdown aparezca delante del modal */
+    .select2-container--open .select2-dropdown {
+        z-index: 2000 !important;
+    }
+</style>
+@endsection
+ 
+@section('content')
+<!-- Encabezado -->
+    <div class="header_section">
+        <div class="bg-transparent mb-3" style="height: 67px"></div>
+        <div class="container-fluid">
+            <div class="" data-aos="fade-right">
+                <h1 class="titulo h2 text-uppercase fw-bold mb-0">ORDEN DE COMPRAS</h1>
+                <div class="" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a class="text-decoration-none link" href="">Principal</a></li>
+                        <li class="breadcrumb-item"><a class="text-decoration-none link" href="{{ url('admin-ordencompras') }}">Orden de Compras</a></li>
+                        <li class="breadcrumb-item link" aria-current="page">Inicio</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- fin encabezado -->
+
+{{-- Contenido --}}
+    <div class="container-fluid">
+        <div class="card border-4 borde-top-secondary shadow-sm h-100" style="border-radius: 20px; min-height: 500px" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            <div class="card-header bg-transparent">
+                <div class="row justify-content-beetween">
+                    <div class="col-md-3 d-flex">
+                        <a type="button" href="{{ url('admin-ordencompras/create') }}" class="btn btn-dark text-uppercase text-white btn-sm w-100">
+                            <i class="bi bi-plus-circle-fill me-2"></i>
+                            Nuevo Registro
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="mb-2 col-12 col-md-6">
+                    <span class="text-uppercase">Total de registros encontrados: <span class="fw-bold">{{ $admin_ordencompras->count() }}</span></span>
+                </div>
+                <table id="display" class="table table-hover table-sm text-center" cellspacing="0" style="width:100%">
+                    <thead class="bg-dark text-white border-0">
+                        <tr>
+                            <th class="h6 small text-center text-uppercase fw-bold">N°</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Codigo</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Proveedor</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Total</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Estado</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Estado Proceso</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Estado Pago</th>
+                            <th class="h6 small text-center text-uppercase fw-bold">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $contador = 1;
+                        @endphp
+                        @foreach($admin_ordencompras as $admin_ordencompra)
+                            <tr>
+                                <td class="fw-normal text-center align-middle">{{ $contador }}</td>
+                                <td class="fw-normal text-center align-middle">{{ $admin_ordencompra->codigo }}</td>
+                                <td class="fw-normal text-center align-middle">{{ $admin_ordencompra->proveedor->persona->name }}</td>
+                                <td class="fw-normal text-center align-middle">{{ $admin_ordencompra->total }}</td>
+                                <td class="fw-normal align-middle">
+                                    @if($admin_ordencompra->estado == 'Inventariado')
+                                        <span class="badge bg-dark border-0">{{ $admin_ordencompra->estado }}</span>
+                                    @elseif($admin_ordencompra->estado == 'En progreso')
+                                        <span class="badge bg-info border-0">{{ $admin_ordencompra->estado }}</span>
+                                    @else
+                                        <span class="badge bg-warning border-0">{{ $admin_ordencompra->estado }}</span>
+                                    @endif
+                                </td>    
+                                <td class="fw-normal text-center align-middle">
+                                    @if($admin_ordencompra->estado_proceso == 'Completado')    
+                                        <span class="badge bg-success border-0">{{ $admin_ordencompra->estado_proceso }}</span>
+                                    @else
+                                        <span class="badge bg-warning border-0">{{ $admin_ordencompra->estado_proceso }}</span>
+                                    @endif
+                                </td>
+                                <td class="fw-normal text-center align-middle">
+                                    @if($admin_ordencompra->estado_pago == 'Pendiente') 
+                                        <span class="badge bg-warning border-0">{{ $admin_ordencompra->estado_pago }}</span>
+                                    @else   
+                                        <span class="badge bg-success border-0">{{ $admin_ordencompra->estado_pago }}</span>
+                                    @endif
+                                </td>
+                                <td class="align-middle">                                        
+                                    <div class="dropstart">
+                                        <button class="btn btn-sm btn-light rounded-circle shadow-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="width: 36px; height: 36px; padding: 0;">
+                                            <i class="bi bi-three-dots-vertical"></i>
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-end shadow"> 
+                                            <li>
+                                                <a href="{{ url("/admin-ordencompras/$admin_ordencompra->slug") }}" class="dropdown-item d-flex align-items-center"><i class="bi bi-eye text-secondary me-2"></i>Detalles</a>
+                                            </li> 
+                                            @if($admin_ordencompra->estado == 'Completado')
+                                            @else
+                                                <li>
+                                                    <a href="{{ url("/admin-ordencompras/$admin_ordencompra->slug/edit") }}" class="dropdown-item d-flex align-items-center"><i class="bi bi-pencil text-secondary me-2"></i>Editar</a>
+                                                </li>
+                                            @endif
+                                            <li><hr class="dropdown-divider"></li>
+                                            <li>
+                                                <form method="POST" action="{{ route('admin-ordencompras.destroy',$admin_ordencompra->slug) }}" class="form-delete">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="dropdown-item d-flex align-items-center text-danger"><i class="bi bi-trash me-2"></i>Eliminar</button>        
+                                                </form>                                                     
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                            @php
+                                $contador++;
+                            @endphp
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+{{-- Fin contenido --}}
+@endsection
+
+@section('js')
+    <!--sweet alert agregar-->
+    @if(session('new_registration') == 'ok')
+    <script>
+        Swal.fire({
+        icon: 'success',
+        confirmButtonColor: '#1C3146',
+        title: '¡Éxito!',
+        text: 'Nuevo registro guardado correctamente',
+        })
+    </script>
+    @endif
+
+    @if(session('exists') == 'ok')
+        <script>
+            Swal.fire({
+            icon: 'warning',
+            confirmButtonColor: '#1C3146',
+            title: '¡Lo sentimos!',
+            text: 'Este registro ya existe',
+            })
+        </script>
+    @endif
+
+    <!--sweet alert actualizar-->
+    @if(session('error') == 'ok')
+        <script>
+            Swal.fire({
+            icon: 'warning',
+            confirmButtonColor: '#1C3146',
+            title: '¡Lo sentimos!',
+            text: 'Este registro no se puede eliminar porque está siendo utilizado en otro registro',
+            })
+        </script>
+    @endif
+
+    <!--sweet alert actualizar-->
+    @if(session('update') == 'ok')
+        <script>
+            Swal.fire({
+            icon: 'success',
+            confirmButtonColor: '#1C3146',
+            title: '¡Actualizado!',
+            text: 'Registro actualizado correctamente',
+            })
+        </script>
+    @endif
+
+    <!--sweet alert eliminar-->
+    @if(session('delete') == 'ok')
+        <script>
+        Swal.fire({
+            icon: 'success',
+            confirmButtonColor: '#1C3146',
+            title: '¡Eliminado!',
+            text: 'Registro eliminado correctamente',
+            })
+        </script>
+    @endif
+    <script>
+        $('.form-delete').submit(function(e){
+            e.preventDefault();
+
+            Swal.fire({
+            title: '¿Estas seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#1C3146',
+            cancelButtonColor: '#FF9C00',
+            confirmButtonText: '¡Sí, eliminar!',
+            cancelButtonText: 'Cancelar'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                
+            this.submit();
+            }
+            })
+
+        });
+    </script>
+    <script>
+        $(document).ready(function(){
+        @if($message = Session::get('errors'))
+            $("#createcategoria").modal('show');
+        @endif
+        });
+    </script>
+
+    <script>
+        (function () {
+        'use strict'
+        var forms = document.querySelectorAll('.needs-validation')
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+            })
+        })()
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            // Inicializar Select2 dentro de modales con dropdownParent
+            $('.modal').on('shown.bs.modal', function() {
+                $(this).find('.select2').select2({
+                    dropdownParent: $(this)
+                });
+            });
+            
+            // Inicializar Select2 fuera de modales
+            $('.select2').filter(function() {
+                return $(this).closest('.modal').length === 0;
+            }).select2();
+        });
+    </script>
+
+    <script>
+        
+    </script>
+@endsection
---
+++ resources/views/ADMINISTRADOR/compras/oservicios/show.blade.php
@@ -0,0 +1,200 @@
+@extends('TEMPLATES.administrador')
+
+@section('title', 'ÓRDENES DE SERVICIOS')
+
+@section('css')
+@endsection
+
+@section('content')
+<!-- Encabezado -->
+    <div class="header_section">
+        <div class="bg-transparent mb-3" style="height: 67px"></div>
+        <div class="container-fluid">
+            <div class="" data-aos="fade-right">
+                <h1 class="titulo h2 text-uppercase fw-bold mb-0">ORDEN DE SERVICIOS</h1>
+                <div class="" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a class="text-decoration-none link" href="">Principal</a></li>
+                        <li class="breadcrumb-item"><a class="text-decoration-none link" href="{{ url('admin-ordenservicios') }}">Órdenes de servicios</a></li>
+                        <li class="breadcrumb-item" aria-current="page">{{ $admin_ordenservicio->codigo }}</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- fin encabezado -->
+    
+    <div class="container-fluid">
+        <div class="card border-4 borde-top-secondary shadow-sm h-100" style="border-radius: 20px; min-height: 500px" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            <div class="card-body text-center">
+                <div class="row g-1">
+                    <div class="col-12 col-md-2 col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Código</p>
+                            </div>
+                            <div class="card-body py-1">
+                                <p class="fw-normal mb-0">{{ $admin_ordenservicio->codigo }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 col-lg-5">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Registrado por</p>
+                            </div>
+                            <div class="card-body py-1">
+                                <p class="fw-normal mb-0">{{ $admin_ordenservicio->registrado_por }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-2 col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Motivo</p>
+                            </div>
+                            <div class="card-body py-1">
+                                <p class="fw-normal mb-0">{{ $admin_ordenservicio->motivo }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-2 col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Fecha</p>
+                            </div>
+                            <div class="card-body py-1">
+                                <p class="fw-normal mb-0">{{ $admin_ordenservicio->fecha }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Codigo de Venta</p>
+                            </div>
+                            <div class="card-body py-1">
+                                <p class="fw-normal mb-0">{{ $admin_ordenservicio->codigo_venta?$admin_ordenservicio->codigo_venta:'Por asignar' }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Forma de pago</p>
+                            </div>
+                            <div class="card-body py-1">
+                                <p class="fw-normal mb-0">{{ $admin_ordenservicio->formapago }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card mb-3">
+                            <div class="card-header py-1">
+                                <p class="small text-uppercase mb-0">Plazo Pago</p>
+                            </div>
+                            <div class="card-body py-1">
+                                @if($admin_ordenservicio->plazo_pago == '0')
+                                    <p class="fw-normal mb-0">CONTADO</p>
+                                @elseif($admin_ordenservicio->plazo_pago == '15')
+                                    <p class="fw-normal mb-0">15 Dias</p>
+                                @elseif($admin_ordenservicio->plazo_pago == '30')
+                                    <p class="fw-normal mb-0">30 Dias</p>
+                                @else
+                                    <p class="fw-normal mb-0">60 Dias</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-12 col-md-12 col-lg-12 mb-3 mb-lg-0">
+                        <div class="table-responsive mt-3">
+                            <table class="table table-sm table-hover text-center" id="detalles-compras">
+                                <thead class="table-light">
+                                <tr>
+                                    <th class="fw-bold small text-uppercase">N°</th>
+                                    <th class="fw-bold small text-uppercase">Tipo</th>
+                                    <th class="fw-bold small text-uppercase">Descripcion</th>
+                                    <th class="fw-bold small text-uppercase">Precio</th>
+                                    <th class="fw-bold small text-uppercase">Tiempo</th>
+                                    <th class="fw-bold small text-uppercase">Vigente HASTA</th>
+                                </tr>
+                                </thead>
+                                <tbody >
+                                    @php
+                                        $contador = 1;
+                                    @endphp
+                                    @foreach($dtlleservicio as $dtlleservicios)
+                                        <tr class="text-center">
+                                            <td class="fw-normal align-middle">{{ $contador }}</td>
+                                            <td class="fw-normal align-middle">{{ $dtlleservicios->tipo_servicio }}</td>
+                                            <td class="fw-normal align-middle">{{ $dtlleservicios->servicio }}</td>
+                                            <td class="fw-normal align-middle">{{ $dtlleservicios->precio }}</td>
+                                            <td class="fw-normal align-middle">{{ $dtlleservicios->tiempo_meses == 1?$dtlleservicios->tiempo_meses.' Mes':$dtlleservicios->tiempo_meses.' Meses' }}</td>
+                                            <td class="fw-normal align-middle">{{ $dtlleservicios->vigencia }}</td>
+                                        </tr>
+                                    @php
+                                        $contador++;
+                                    @endphp
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-beetween">
+                    <div class="col-12 col-md-6">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <p>{{ $admin_ordenservicio->nota }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-12 col-md-5">
+                        <table class="w-100">
+                            <tr>
+                                <td class="border-0 ps-2 py-1 bg-light" style="width: 50%">
+                                    Subtotal
+                                </td>
+                                <td class="border-0 pe-2 py-1 bg-light" style="width: 50%">
+                                    <div class="clearfix">
+                                        <span class="float-start ps-2">S/ </span>
+                                        <span class="float-end" id="subtotal_id">
+                                            {{ $admin_ordenservicio->total }}
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-0 fw-bold ps-2 py-1 bg-light" style="width: 50%">
+                                    TOTAL
+                                </td>
+                                <td class="border-0 fw-bold pe-2 py-1 bg-light" style="width: 50%">
+                                    <div class="clearfix">
+                                        <span class="float-start ps-2">S/ </span>
+                                        <span class="float-end"id="total_id">
+                                            {{ $admin_ordenservicio->total }}
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="p-3 text-end" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            <a href="{{ url('admin-ordenservicios') }}" class="btn btn-outline-secondary px-5">Volver</a>
+        </div>     
+    </div>
+            
+
+@endsection
+
+@section('js')
+
+@endsection
